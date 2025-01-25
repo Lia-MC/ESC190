07001ec4995354729b57366b270c9f82e5436bee
Lia-MC <lia.mc73700@gmail.com>
2025-01-24 19:29:51 -0500

    Minor changes saved before new laptop

---
+++ student.h
@@ -0,0 +1,14 @@
+#if !defined(STUDENT_H)
+#define STUDENT_H
+
+typedef struct student{
+    char name[20];
+    int age;
+    double gpa;
+} student;
+
+// header file
+
+#endif
+
+// this is called an include guard
