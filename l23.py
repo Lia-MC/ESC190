61cfb908f8eb1c33f836a40471acc9194b9a16cd
Lia-MC <lia.mc73700@gmail.com>
2025-03-21 09:18:17 -0400

    dfs rec code

---
+++ l23.py
@@ -0,0 +1,12 @@
+def DFS_rec(g, cur, visited = None):
+    if visited == None:
+        visited = [False] * g.num_nodes
+    
+    cur_i = g.node_names[cur]
+    cur_neighbour = g.nodes[cur_i].head
+    visited[cur_i] = True
+    print(cur)
+    while cur_neighbour:
+        if not visited[cur_neighbour.data]:
+            DFS_rec(g, g.node_names_rev[cur_neighbour.data], visited)
+        cur_neighbour = cur_neighbour.next
