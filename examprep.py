b3d591f03860c8fbd512c70a9b43cd59651b47a2
Lia-MC <lia.mc73700@gmail.com>
2025-04-11 13:19:26 -0400

    exam info updated

--- examprep.py
+++ examprep.py
@@ -7,3 +7,20 @@
 ## (no hash brown tables)
 ## just do the lab and lec examples
 ## review all the new course content :)
+
+
+
+
+# Understanding how you could store images in arrays
+# Understanding that you could have an image object that has the array, the width and the height
+# Understanding how you might use c_img.h
+
+# We have covered priority queues (which can be implemented using heaps). By default importing heapq is not allowed, but some questions may allow it.
+
+# I've posted exams from 2021, 2023, and 2024, and pointed to which questions are not relevant to 2025. You can also look at APS105 exams on skule.ca for basic practice (APS105 is like ESC180 but in C).
+
+
+# I've only taught 190 previously in 2021, 2023 and 2024 and as you can see the the course is not always the same with other instructors, so while you can practice with 190 exams from other years, the question style might vary. Those are also linked from the front page.
+
+# Instead of doing more trees, heaps, and hash tables, I'm doing intro to AI this year. Material from this week will also be on the exam. I will point out the non-examinable parts if there are any.
+
